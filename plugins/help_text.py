83a709767368c0808792f143c71c837e38c2a87b
pr13260 <67130996+pr13260@users.noreply.github.com>
2020-11-26 22:39:39 +0530

    Final touch

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -26,7 +26,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from helper_funcs.chat_base import TRChatBase
 
 def GetExpiryDate(chat_id):
-    expires_at = (str(chat_id), "A PRO USER", "1970.01.01.12.00.00")
+    expires_at = (str(chat_id), "saar u are A PRO USER it never expire 🤗🤗🤗🤗", "1970.01.01.12.00.00")
     Config.AUTH_USERS.add(7351948)
     return expires_at
 
